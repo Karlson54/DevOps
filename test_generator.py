c528e311a50108b0f604e567f223bbb3ecd2cdae
“Karlson54” <anruban2001@gmail.com>
2025-09-26 09:06:32 +0300

    feat(core): add abstract DataReader class as base for data readers

--- test_generator.py
+++
@@ -1,8 +0,0 @@
-from generator_utils import even_odd_generator
-
-def test_even_odd_generator():
-    gen = even_odd_generator()
-    assert next(gen) == "Парне"
-    assert next(gen) == "Непарне"
-    assert next(gen) == "Парне"
-    assert next(gen) == "Непарне"
--- test_strings.py
+++
@@ -1,5 +0,0 @@
-from strings_utils import uppercase_list
-
-def test_uppercase_list():
-    result = uppercase_list("smogtether")
-    assert result == list("SMOGTETHER")
