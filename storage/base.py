e4b940373ce86d2049b297fa952e708afbebe8c5
“Karlson54” <anruban2001@gmail.com>
2025-10-16 18:30:13 +0300

    chore: initialize project

--- storage/base.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-
-class DataStorage(ABC):
-    """Абстрактний клас ЗБЕРЕЖЕННЯ_ДАНИХ."""
-    @abstractmethod
-    def save(self, students_data, filename: str) -> str:
-        """Зберігає дані студентів у файл."""
-        pass
--- utils/filename.py
+++
@@ -1,7 +0,0 @@
-def filename_from_student(student, work_number: int, fmt: str, multiple: bool = False) -> str:
-    """Формування імені файлу за шаблоном: Прізвище_Ім’я_ПоБатькові_Група_PRN.формат"""
-    base = f"{student.last_name}_{student.first_name}_{student.patronymic}_{student.group_number}"
-    if multiple:
-        base += "_декілька"
-    base += f"_PR{work_number}"
-    return f"{base}.{fmt.lower()}"
