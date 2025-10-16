2dbdfb74d09b1667396adfe5cd6cda845a42410f
“Karlson54” <anruban2001@gmail.com>
2025-10-16 21:53:57 +0300

    feat(egg): add falling animation method

--- Egg.py
+++ Egg.py
@@ -11,3 +11,16 @@ class Egg:
         self.canvas.move(self.id, random.randint(10, 490), -10)
         
         self.y = random.randint(1, 4)
+
+    def draw(self):
+        
+        self.canvas.move(self.id, 0, self.y)
+        
+        pos = self.canvas.coords(self.id)
+        
+        if pos[3] >= self.canvas.winfo_height():
+            self.score.lost_egg()
+            self.canvas.delete(self.id)
+            return 'hit bottom'
+        
+        return None
