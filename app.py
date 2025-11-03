7f9d5361160ec4a1d4b0db17d296aa54947d98dc
“Karlson54” <anruban2001@gmail.com>
2025-11-03 17:29:32 +0200

    feat: add /error route with intentional exception

--- app.py
+++ app.py
@@ -20,3 +20,21 @@ def index():
     request_count += 1
     logging.info("Main route accessed")
     return "Сервіс працює"
+
+def send_to_statsd(message):
+    """Send error message to UDP StatsD server"""
+    UDP_IP = "127.0.0.1"
+    UDP_PORT = 9999
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.sendto(message.encode(), (UDP_IP, UDP_PORT))
+
+@app.route('/error')
+def error():
+    global request_count
+    request_count += 1
+    try:
+        1 / 0  # викликаємо помилку
+    except Exception as e:
+        logging.exception("An error occurred!")
+        send_to_statsd(f"Error occurred: {str(e)}")
+        return "Помилка оброблена. Перевірте логи.", 500
