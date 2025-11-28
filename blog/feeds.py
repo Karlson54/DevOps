a72efb3aad1d2f0b54effeb93eedbce0d9f6a6c4
“Karlson54” <anruban2001@gmail.com>
2025-11-28 20:38:57 +0200

    feat: package install, replace files

--- blog/feeds.py
+++ blog/feeds.py
@@ -2,8 +2,7 @@ import markdown
 from django.contrib.syndication.views import Feed
 from django.template.defaultfilters import truncatewords_html
 from django.urls import reverse_lazy
-from .models import Post
-
+from blog.models import Post
 
 class LatestPostsFeed(Feed):
     title = "My blog"
